5b998d54b2c7cae0a1a0951d911b07a95eaed1ae
Natthaphon Hongcharoen <palm22180@gmail.com>
2023-08-17 16:35:41 +0700

    init

---
+++ dataset/ocr_data.py
@@ -0,0 +1,100 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+import os
+from torch.utils.data import Dataset
+from .transforms import CVColorJitter, CVDeterioration, CVGeometry
+from torchvision import transforms
+import cv2
+import math
+import random
+from PIL import Image
+import numpy as np
+import json
+
+
+class ImageDataset(Dataset):
+    def __init__(self,
+                 src: str,
+                 jsonl: str,
+                 is_training: bool = True,
+                 img_h: int = 32,
+                 img_w: int = 100,
+                 data_aug: bool = True,
+                 multiscales: bool = True,
+                 convert_mode: str = 'RGB',
+                 ):
+        self.data_aug = data_aug
+        self.convert_mode = convert_mode
+        self.img_h = img_h
+        self.img_w = img_w
+        self.multiscales = multiscales
+        self.is_training = is_training
+        self.src = src
+        if not os.path.exists(jsonl):
+            self.data = ['{"filename": "./dataset/images/000000.png", "text": "4"}'] * 100
+        else:
+            self.data = open(jsonl).read().split('\n')[:-1]
+        if self.is_training and self.data_aug:
+            self.augment_tfs = transforms.Compose([
+                CVGeometry(degrees=45, translate=(0.0, 0.0), scale=(0.5, 2.), shear=(45, 15), distortion=0.5, p=0.5),
+                CVDeterioration(var=20, degrees=6, factor=4, p=0.25),
+                CVColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1, p=0.25)
+            ])
+        self.totensor = transforms.ToTensor()
+
+    def __len__(self):
+        return len(self.data)
+
+    def resize_multiscales(self, img, borderType=cv2.BORDER_CONSTANT):
+        def _resize_ratio(img, ratio, fix_h=True):
+            if ratio * self.img_w < self.img_h:
+                if fix_h:
+                    trg_h = self.img_h
+                else:
+                    trg_h = int(ratio * self.img_w)
+                trg_w = self.img_w
+            else:
+                trg_h, trg_w = self.img_h, int(self.img_h / ratio)
+            img = cv2.resize(img, (trg_w, trg_h))
+            pad_h, pad_w = (self.img_h - trg_h) / 2, (self.img_w - trg_w) / 2
+            top, bottom = math.ceil(pad_h), math.floor(pad_h)
+            left, right = math.ceil(pad_w), math.floor(pad_w)
+            img = cv2.copyMakeBorder(img, top, bottom, left, right, borderType)
+            return img
+
+        if self.is_training:
+            if random.random() < 0.5:
+                base, maxh, maxw = self.img_h, self.img_h, self.img_w
+                h, w = random.randint(base, maxh), random.randint(base, maxw)
+                return _resize_ratio(img, h / w)
+            else:
+                return _resize_ratio(img, img.shape[0] / img.shape[1])  # keep aspect ratio
+        else:
+            return _resize_ratio(img, img.shape[0] / img.shape[1])  # keep aspect ratio
+
+    def resize(self, img):
+        if self.multiscales:
+            return self.resize_multiscales(img, cv2.BORDER_REPLICATE)
+        else:
+            return cv2.resize(img, (self.img_w, self.img_h))
+
+    def get(self, idx):
+        data = json.loads(self.data[idx])
+        im = data['filename']
+        label = data['text']
+        # image = Image.open(os.path.join(self.f, im)).convert(self.convert_mode)
+        return os.path.join(self.src, im), label, idx
+
+    def _process_training(self, image):
+        if self.data_aug:
+            image = self.augment_tfs(image)
+        image = self.resize(np.array(image))
+        return image
+
+    def _process_test(self, image):
+        return self.resize(np.array(image))
+
+    def __getitem__(self, idx):
+        image, text, idx_new = self.get(idx)
+        return image, text
---
+++ train.py
@@ -0,0 +1,168 @@
+import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = ''
+import torch
+from transformers import ViTImageProcessor, AutoTokenizer, VisionEncoderDecoderModel
+import nltk
+import evaluate
+import numpy as np
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+from dataset.ocr_data import ImageDataset
+from PIL import Image
+
+
+def tokenization_fn(captions, max_target_length=120):
+    """Run tokenization on captions."""
+    labels = tokenizer(captions,
+                       padding="max_length",
+                       max_length=max_target_length,
+                       return_tensors="pt",
+                       truncation=True).input_ids
+
+    return labels
+
+
+def feature_extraction_fn(image_paths):
+    images = [Image.open(image_file) for image_file in image_paths]
+
+    encoder_inputs = feature_extractor(images=images, return_tensors="pt")
+
+    return encoder_inputs.pixel_values
+
+
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [label.strip() for label in labels]
+    # rougeLSum expects newline after each sentence
+    preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
+    labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
+    return preds, labels
+
+
+def collate_fn(batch):
+    model_inputs = {'labels': [], 'pixel_values': []}
+    for obj in batch:
+        model_inputs['labels'].append(obj[1])
+        model_inputs['pixel_values'].append(obj[0]['img_path'])
+    model_inputs['labels'] = tokenization_fn(model_inputs['labels'])
+    model_inputs['pixel_values'] = feature_extraction_fn(model_inputs['pixel_values'])
+    return model_inputs
+
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    if ignore_pad_token_for_loss:
+        # Replace -100 in the labels as we can't decode them.
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    # Some simple post-processing
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds,
+                                                     decoded_labels)
+    rouge_result = rouge.compute(predictions=decoded_preds,
+                                 references=decoded_labels,
+                                 use_stemmer=True)
+    result = {k: round(v * 100, 4) for k, v in rouge_result.items()}
+    # bleu_result = bleu.compute(predictions=decoded_preds,
+    #                            references=decoded_labels)
+    # result.update({k: round(v * 100, 4) for k, v in bleu_result.items()})
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    return result
+
+
+if __name__ == '__main__':
+    max_per_img = 50
+
+    if os.path.exists("/project/lt200060-capgen/coco"):
+        vit_model = "/project/lt200060-capgen/palm/huggingface/vit-large-patch16-384"
+        text_decode_model = "/project/lt200060-capgen/palm/huggingface/mGPT"
+        src_dir = "/project/lt200060-capgen/peune/ocr"
+        train_jsonl = '/project/lt200060-capgen/coco/annotations/captions_train2017.json'
+        val_jsonl = '/project/lt200060-capgen/coco/annotations/captions_val2017.json'
+        log_output_dir = "/project/lt200060-capgen/palm/hf-captioning/dino-pre-bbox"
+        config_file = '/home/nhongcha/mmdetection/configs/dino/dino-4scale_r50_8xb2-12e_coco.py'
+        detector_weight = '/project/lt200060-capgen/palm/pretrained/dino-4scale_r50_8xb2-12e_coco_20221202_182705-55b2bba2.pth'
+        output_dir = os.path.join('/project/lt200060-capgen/palm/hf-captioning/baseline-l')
+        bs = 16
+        workers = 4
+    elif os.path.exists("/media/palm/Data/capgen/"):
+        vit_model = "google/vit-base-patch16-224-in21k"
+        text_decode_model = "ai-forever/mGPT"
+        src_dir = "/media/palm/Data/ocr/"
+        train_jsonl = '/project/lt200060-capgen/coco/annotations/captions_train2017.json'
+        val_jsonl = '/project/lt200060-capgen/coco/annotations/captions_val2017.json'
+        config_file = '/home/palm/PycharmProjects/mmdetection/configs/dino/dino-4scale_r50_8xb2-12e_coco.py'
+        detector_weight = ''
+        log_output_dir = "/media/palm/Data/capgen/out"
+        output_dir = os.path.join('tmp/baseline')
+        bs = 1
+        workers = 0
+    else:
+        vit_model = "google/vit-base-patch16-224-in21k"
+        text_decode_model = "ai-forever/mGPT"
+        src_dir = "/media/palm/Data/ocr/"
+        train_jsonl = '/project/lt200060-capgen/coco/annotations/captions_train2017.json'
+        val_jsonl = '/project/lt200060-capgen/coco/annotations/captions_val2017.json'
+        log_output_dir = "/tmp/out"
+        config_file = '/home/palm/PycharmProjects/mmdetection/configs/dino/dino-4scale_r50_8xb2-12e_coco.py'
+        detector_weight = '/home/palm/PycharmProjects/mmdetection/cp/dino-4scale_r50_8xb2-12e_coco_20221202_182705-55b2bba2.pth'
+        output_dir = os.path.join('tmp/baseline')
+        bs = 2
+        workers = 0
+    rouge = evaluate.load("rouge")
+    bleu = evaluate.load("bleu")
+    ignore_pad_token_for_loss = True
+
+    model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(vit_model, text_decode_model)
+    feature_extractor = ViTImageProcessor.from_pretrained(vit_model)
+    tokenizer = AutoTokenizer.from_pretrained(text_decode_model)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # update the model config
+    model.config.eos_token_id = tokenizer.eos_token_id
+    model.config.decoder_start_token_id = tokenizer.bos_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
+    model.save_pretrained(output_dir)
+    # feature_extractor.save_pretrained(output_dir)
+    tokenizer.save_pretrained(output_dir)
+
+    train_set = ImageDataset(
+        src_dir,
+        train_jsonl
+    )
+    print(len(train_set), flush=True)
+    valid_set = ImageDataset(
+        src_dir,
+        val_jsonl
+    )
+    print(len(valid_set), flush=True)
+    # train_loader = DataLoader(train_set, **train_hyperparams)
+    # valid_loader = DataLoader(valid_set, **valid_hyperparams)
+
+    training_args = Seq2SeqTrainingArguments(
+        predict_with_generate=True,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        save_total_limit=1,
+        per_device_train_batch_size=bs,
+        per_device_eval_batch_size=bs,
+        num_train_epochs=12,
+        output_dir=log_output_dir,
+        dataloader_num_workers=workers,
+        logging_strategy='steps',
+        logging_steps=100,
+        disable_tqdm=True,
+        # report_to=['tensorboard']
+    )
+    trainer = Seq2SeqTrainer(
+        model=model,
+        # tokenizer=feature_extractor,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=train_set,
+        eval_dataset=valid_set,
+        data_collator=collate_fn,
+    )
+    trainer.train()
