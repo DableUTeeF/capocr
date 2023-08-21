94fc32ae54c76971722cd0d48430404121e2f07e
DableUTeeF <palm22180@gmail.com>
2023-08-21 10:23:44 +0700

    a

--- inference.py
+++ inference.py
@@ -2,4 +2,4 @@ from transformers import pipeline
 
 if __name__ == '__main__':
     image_captioner = pipeline("image-to-text", model="workdir/mgpt_vit", device='gpu')
-    image_captioner("imgs/19.jpg")
+    print(image_captioner("imgs/19.jpg"))
