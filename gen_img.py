dee114843e91ab7e14d0ca0d7f2b3d1e096e6db3
Natthaphon Hongcharoen <palm22180@gmail.com>
2023-08-23 00:56:50 +0700

    Added gen_img

---
+++ gen_img.py
@@ -0,0 +1,119 @@
+import numpy as np
+from PIL import Image, ImageFont, ImageDraw
+import glob
+import cv2
+import re
+
+
+def deemojify(text):
+    "function to remove emojis from text"
+    regrex_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002500-\U00002BEF"  # chinese char
+                                u"\U00002702-\U000027B0"
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                u"\U0001f926-\U0001f937"
+                                u"\U00010000-\U0010ffff"
+                                u"\u2640-\u2642"
+                                u"\u2600-\u2B55"
+                                u"\u200d"
+                                u"\u23cf"
+                                u"\u23e9"
+                                u"\u231a"
+                                u"\ufe0f"  # dingbats
+                                u"\u3030"
+                                "]+", re.UNICODE)
+    return regrex_pattern.sub(r'', text)
+
+
+def create_text_generator(txtdir):
+    filelist = glob.glob('%s/*.txt' % txtdir)
+    for j, f in enumerate(filelist):
+        file = open(f, "r")
+        for i, line in enumerate(file):
+            line = deemojify(line.rstrip('\n'))
+            for k in range(0, len(line), 100):  # skip 150 per chunk
+                # random length between 10-150
+                l = 10 + np.random.randint(90)
+                yield line[k:k + l]
+
+
+def random_example(
+        text,
+        font_list,
+        font_size_min=14, font_size_max=40,
+        max_height=50, max_width=1200,
+):
+    # random
+    font_idx = np.random.randint(len(font_list))
+    font_size = np.random.randint(font_size_min, high=font_size_max)
+    for _ in range(len(text)):
+        font = ImageFont.truetype(font_list[font_idx], font_size)  # load font
+
+        image = Image.new("L", (500, 500), "white")
+        draw = ImageDraw.Draw(image)
+
+        # get the size of the text
+        # text_size = draw.textsize(text, font)
+        bbox = draw.textbbox(xy=(0, 0), text=text, font=font)
+        if bbox[2] <= 0 or bbox[3] <= 0:
+            print("Error (%s)" % text)
+            return None, None
+
+        # resize and draw
+        image = image.resize((bbox[2], bbox[3]))
+        draw = ImageDraw.Draw(image)
+        draw.text((0, 0), text, 0, font=font)
+
+        w, h = image.size
+        H = max_height
+        W = int(max_height * w / h)
+        if W <= max_width:
+            image = image.resize((W, H))
+            break
+        else:  # too long -> reduce font ize & shorten text
+            font_size -= 1
+            if font_size < font_size_min:
+                font_size = font_size_min
+            text = text[:-1]
+
+    result = Image.new("L", (max_width, max_height), "white")
+    result.paste(image, (0, 0))
+
+    return np.asarray(result), text
+
+
+###################################################################################
+
+import argparse
+import glob
+from tqdm import tqdm
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('txt', type=str, help="Text folder")
+    parser.add_argument('ttf', type=str, help="Font folder")
+    parser.add_argument('out', type=str, help="Out folder")
+    args = parser.parse_args()
+
+    font_list = glob.glob('%s/*.ttf' % args.ttf)
+
+    with open(f"{args.out}/annotations", "w") as file:
+        j = 0
+        for text_org in tqdm(create_text_generator(args.txt)):
+            img, text = random_example(text_org, font_list)
+            if img is None: continue
+
+            filename = f"{args.out}/images/%.6d.png" % j
+            j += 1
+            cv2.imwrite(filename, img)
+            file.write("{\"filename\": \"%s\", \"text\": \"%s\"}\n" % (filename, text))
+            file.flush()
+
+            if j >= 999999:
+                break
