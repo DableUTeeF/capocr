7c589997550705fbebdbed7f86284b2d8b26ca59
Natthaphon Hongcharoen <palm22180@gmail.com>
2023-08-23 00:45:47 +0700

    Now use real sample

--- inference.py
+++ inference.py
@@ -13,7 +13,7 @@ def preprocess(image, image_processor):
 
 def forward(model, model_inputs, generate_kwargs=None):
     if generate_kwargs is None:
-        generate_kwargs = {}
+        generate_kwargs = {'max_new_tokens': 100}
     inputs = model_inputs.pop(model.main_input_name)
     model_outputs = model.generate(inputs, **model_inputs, **generate_kwargs)
     return model_outputs
@@ -39,11 +39,13 @@ def generate(inputs):
 
 
 if __name__ == '__main__':
+    image_path = '/home/palm/PycharmProjects/mmmmocr/imgs/samples/m.png'
+    im = Image.open(image_path)
     model = VisionEncoderDecoderModel.from_pretrained("/media/palm/Data/ocr/cap/train/checkpoint-367500")
     image_processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
     tokenizer = AutoTokenizer.from_pretrained("ai-forever/mGPT")
 
-    generate('/media/palm/Data/ocr/data/images/002346.png')
+    generate(image_path)
 
     image_captioner = pipeline("image-to-text", model="workdir/mgpt_vit", device='gpu')
     print(image_captioner("imgs/19.jpg"))
