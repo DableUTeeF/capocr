60ede6a991d773d5e07fc075c868774aee619766
Natthaphon Hongcharoen <nhongcha@x3002c0s9b0n0.head.lanta.nstda.or.th>
2023-08-27 13:07:26 +0700

    Added train.sh

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 -c 16                      # Specify number of nodes and processors per task
 #SBATCH --ntasks-per-node=1		        # Specify number of tasks per node
 #SBATCH --gpus=1		                # Specify total number of GPUs
-#SBATCH -t 120:00:00                    # Specify maximum time limit (hour: minute: second)
+#SBATCH -t 80:00:00                    # Specify maximum time limit (hour: minute: second)
 #SBATCH -A lt200060                     # Specify project name
 #SBATCH -J cap_train                      # Specify job name
 #SBATCH --error=slurm_outputs/%j.out
@@ -16,5 +16,9 @@ conda activate /project/lt200060-capgen/palm/conda_envs/.conda/envs/palm_caption
 export _TYPER_STANDARD_TRACEBACK=1
 export TYPER_STANDARD_TRACEBACK=1
 export ACCELERATE_DISABLE_RICH=1
+
+git rev-parse HEAD
+
+echo $@
 
 srun python3 main.py $@
